220369fa95d4aa14d5dd27d97c115116c75c1844
josh <joshhsoj1902@gmail.com>
2017-11-05 21:11:31 -0500

    Bind address to the right network

--- ogpmanager.sh
+++ ogpmanager.sh
@@ -3,6 +3,12 @@
 /usr/local/bin/gomplate -f /opt/OGP/templates/Config.pm.tmpl -o /opt/OGP/Cfg/Config.pm
 /usr/local/bin/gomplate -f /opt/OGP/templates/Preferences.pm.tmpl -o /opt/OGP/Cfg/Preferences.pm
 
+bind_gateway=`docker network inspect --format='{{ (index .IPAM.Config 0).Gateway }}' docker_gwbridge`
+
+echo 'bind_gateway: '$bind_gateway > /opt/OGP/Cfg/bind.yml
+
+echo 'The bindGW '$bind_gateway
+
 chmod +x /opt/OGP/ogp_agent_run
 echo "Starting DockerOGP"
 su ogp_agent -c "cd /opt/OGP/; ./ogp_agent_run"
