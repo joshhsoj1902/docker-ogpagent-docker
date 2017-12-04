0e0c465d3ef3e3a727e0f0e93e91b2977cfbdb70
josh <joshhsoj1902@gmail.com>
2017-12-03 22:06:31 -0500

    if those networks

--- OGP-Agent-Linux/ogp_agent.pl
+++ OGP-Agent-Linux/ogp_agent.pl
@@ -716,14 +716,20 @@ sub gomplate_compose
 
   sudo_exec_without_decrypt($gomplate_cmd);
 
+  if( length $bind_ingress ) {
+      my $append_ingress_cmd = 'echo "\n      -bind_ingress=' . $bind_ingress . '" >> ' . $output;
+      sudo_exec_without_decrypt($append_ingress_cmd);
+    }
+  #TODO: this needs to be done better, possibly with  a \n, the spacing is correct though
+
+  if( length $bind_overlay) {
+	  my $append_overlay_cmd = 'echo "\n      -bind_overlay=' . $bind_overlay . '" >> ' . $output;
+      sudo_exec_without_decrypt($append_overlay_cmd);
+    }
+
+
   my $append_env_cmd = 'cat ' . $game_instance_dir . '/docker-environment.yml >> ' . $output;
   sudo_exec_without_decrypt($append_env_cmd);
-
-  my $append_ingress_cmd = 'echo "      -bind_ingress=' . $bind_ingress . '" >> ' . $output;
-  sudo_exec_without_decrypt($append_ingress_cmd);
-
-  my $append_overlay_cmd = 'echo "      -bind_overlay=' . $bind_overlay . '" >> ' . $output;
-  sudo_exec_without_decrypt($append_overlay_cmd);
 
   return 1;
 }
