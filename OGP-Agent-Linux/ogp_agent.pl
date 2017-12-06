a2898103e2611b1fc5573590b19f5cb91824b0e2
josh <joshhsoj1902@gmail.com>
2017-12-05 20:28:09 -0500

    fix typo

--- OGP-Agent-Linux/ogp_agent.pl
+++ OGP-Agent-Linux/ogp_agent.pl
@@ -1012,7 +1012,7 @@ sub stop_server_without_decrypt
 		$control_password, $control_type, $home_path) = @_;
 
 #   my $docker_run_command = 'docker stack rm ' . $home_id;
-  my $docker_run_command = 'docker service scale' . $home_id . '_game=0 ' . ;
+  my $docker_run_command = 'docker service scale' . $home_id . '_game=0 ';
   logger 'docker command: ' . $docker_run_command;
   sudo_exec_without_decrypt($docker_run_command);
   return 0;
