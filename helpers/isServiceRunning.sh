43e7dfcee7928f21c611cee0b35503ce4718dcfb
josh <joshhsoj1902@gmail.com>
2017-12-07 21:16:27 -0500

    Typo again

---
+++ helpers/isServiceRunning.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+service_name=$1
+
+service_count = $(sudo docker service ls --format='{{.Name}}' | grep -oh "$service_name" | wc -w)
+echo 'service count' $service_count
+  # my $docker_replicas = sudo_exec_without_decrypt($docker_service_cmd);
+
+if [ "$service_count" -gt "0"]
+then
+    service_replicas = $(sudo docker service inspect --format='{{.Spec.Mode.Replicated.Replicas}}' $service_name)
+
+else
+
+fi
+
+#   if ($service_count > 0) {
+#     my $docker_service_replicas = `sudo docker service inspect --format='{{.Spec.Mode.Replicated.Replicas}}'' $service_name `;
+
+
+
+# 	if ($docker_service_replicas > 0) {
+# 	  logger 'Service running ' . $docker_service_replicas;
+#       return 1;
+# 	}
+# 	else
+# 	{
+# 	  logger 'Service has no replicas ' . $docker_service_replicas;
+#       return 0;
+# 	}
+#   }
+#   else
+#   {
+#     logger 'Service is not up ' . $docker_service;
+#     return 0;
+#   }
+#   fi
