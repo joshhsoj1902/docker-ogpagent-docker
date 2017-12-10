2756052e80926542a0ed161238bc6853e722d9c3
josh <joshhsoj1902@gmail.com>
2017-12-09 20:57:01 -0500

    fix start script

--- OGP-Agent-Linux/helpers/startService.sh
+++ OGP-Agent-Linux/helpers/startService.sh
@@ -4,4 +4,4 @@ game_dir=$1
 home_id=$2
 game_instance_dir=$game_dir/$home_id
 
-docker stack deploy -c $game_instance_dir '/docker-compose.yml ' $home_id;
+docker stack deploy -c $game_instance_dir'/docker-compose.yml' $home_id;
