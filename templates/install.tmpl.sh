4ff950e1969a49d746ba02968672039fdef07dba
josh <joshhsoj1902@gmail.com>
2017-12-10 18:50:55 -0500

    fix install

--- templates/install.tmpl.sh
+++ templates/install.tmpl.sh
@@ -2,27 +2,25 @@
 
 home_id=$1
 
-configNameSpace={{(datasource "config").namespace}}
-configImage={{(datasource "config").image}}
-configVersion={{(datasource "config").version}}
+configNamespace='{{(datasource "config").namespace}}'
 
-dockerNameSpace={{(datasource "globalConfig").docker_hub_namespace}}
-gcloudNamespace={{(datasource "globalConfig").docker_gcloud_namespace}}
+dockerNamespace='{{(datasource "globalConfig").docker_hub_namespace}}'
+gcloudNamespace='{{(datasource "globalConfig").docker_gcloud_namespace}}'
 
-if [ ${#dockerNameSpace} -gt 0 ] && [ "$configNameSpace" = "$dockerNameSpace" ]; then
+if [ ${#dockerNamespace} -gt 0 ] && [ "$configNamespace" = "$dockerNamespace" ]; then
     echo "Using dockerhub"
     exit 0
     # Dockerhub services can be installed on the worker
 fi
 
-if [ ${#gcloudNamespace} -gt 0 ] && [ "$gcloudNamespace" = "$dockerNameSpace" ]; then
+if [ ${#gcloudNamespace} -gt 0 ] && [ "$configNamespace" = "$gcloudNamespace" ]; then
     echo "Using gcloud"
     docker stack deploy -c docker-compose.install.yml $home_id'_install';
     exit 0
     # image will be downloaded in the background
 fi
 
-if [ ${#dockerNameSpace} -gt 0 ] || [ ${#gcloudNamespace} -gt 0 ]; then
+if [ ${#dockerNamespace} -gt 0 ] || [ ${#gcloudNamespace} -gt 0 ]; then
     echo "Namespace error" #TODO. make this blocking
     exit 1
     # If a namespace is defined, the image must match one of them
