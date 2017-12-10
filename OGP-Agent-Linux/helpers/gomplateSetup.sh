54ef6741250c449f9a7eab01570b685630a50614
josh <joshhsoj1902@gmail.com>
2017-12-09 20:31:55 -0500

    bash>perl

--- OGP-Agent-Linux/helpers/gomplateSetup.sh
+++ OGP-Agent-Linux/helpers/gomplateSetup.sh
@@ -7,23 +7,13 @@ game_instance_dir=$game_dir/$home_id
 echo 'homeid: '$home_id
 echo 'game dir: '$game_instance_dir
 
-gomplate = '/usr/local/bin/gomplate';
-configdatasource = 'config=file://' . $game_instance_dir . '/docker-config.yml';
-binddatasource = 'bind=file://' . '/opt/OGP/Cfg/bind.yml';
-template = '/opt/OGP/templates/docker-compose.tmpl';
-output = $game_instance_dir . '/docker-compose.yml';
+configdatasource='config=file://'$game_instance_dir'/docker-config.yml';
+binddatasource='bind=file:///opt/OGP/Cfg/bind.yml';
+template='/opt/OGP/templates/docker-compose.tmpl';
+output=$game_instance_dir'/docker-compose.yml';
 
 echo 'GOMPLATE: ' -d $configdatasource -d $binddatasource -f $template -o $output
 
 /usr/local/bin/gomplate -d $configdatasource -d $binddatasource -f $template -o $output
 
-cat $game_instance_dir'/docker-environment.yml >> ' $output
-
-#   my $gomplate_cmd = $gomplate . ' -d ' . $configdatasource . ' -d ' . $binddatasource . ' -f ' . $template . ' -o ' . $output;
-
-#   logger 'The gomplate command: ' . $gomplate_cmd;
-
-#   sudo_exec_without_decrypt($gomplate_cmd);
-
-#   my $append_env_cmd = 'cat ' . $game_instance_dir . '/docker-environment.yml >> ' . $output;
-#   sudo_exec_without_decrypt($append_env_cmd);
+cat $game_instance_dir'/docker-environment.yml' >> $output
