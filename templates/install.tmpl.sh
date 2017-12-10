69db070c5d95d316f96a538a7fd07ad7618b7737
josh <joshhsoj1902@gmail.com>
2017-12-10 18:30:30 -0500

    make things a little better

--- templates/install.tmpl.sh
+++ templates/install.tmpl.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+home_id=$1
+
 configNameSpace={{(datasource "config").namespace}}
 configImage={{(datasource "config").image}}
 configVersion={{(datasource "config").version}}
@@ -8,17 +10,20 @@ dockerNameSpace={{(datasource "globalConfig").docker_hub_namespace}}
 gcloudNamespace={{(datasource "globalConfig").docker_gcloud_namespace}}
 
 if [ ${#dockerNameSpace} -gt 0 ] && [ "$configNameSpace" = "$dockerNameSpace" ]; then
+    echo "Using dockerhub"
     exit 0
     # Dockerhub services can be installed on the worker
 fi
 
 if [ ${#gcloudNamespace} -gt 0 ] && [ "$gcloudNamespace" = "$dockerNameSpace" ]; then
-    docker stack deploy -c $game_instance_dir'/docker-compose.install.yml' $home_id'_install';
+    echo "Using gcloud"
+    docker stack deploy -c docker-compose.install.yml $home_id'_install';
     exit 0
     # image will be downloaded in the background
 fi
 
 if [ ${#dockerNameSpace} -gt 0 ] || [ ${#gcloudNamespace} -gt 0 ]; then
+    echo "Namespace error" #TODO. make this blocking
     exit 1
     # If a namespace is defined, the image must match one of them
 fi
