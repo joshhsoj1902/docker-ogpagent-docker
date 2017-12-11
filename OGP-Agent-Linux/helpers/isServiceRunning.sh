9c9153768bf6d7330c1a6ce2097d7dd2d343b7a6
Josh Bryans <joshhsoj1902@gmail.com>
2017-12-10 22:09:32 -0500

    Up was reporting is there should be 1 up (#20)

--- OGP-Agent-Linux/helpers/isServiceRunning.sh
+++ OGP-Agent-Linux/helpers/isServiceRunning.sh
@@ -7,7 +7,13 @@ service_count=$(docker service ls --format='{{.Name}}' | grep -oh "$service_name
 
 if [ "$service_count" -gt "0" ]
 then
-    service_replicas=$(docker service inspect --format='{{.Spec.Mode.Replicated.Replicas}}' $service_name)
+    service_replicas=$(docker service ls --format='{{.Replicas}}' --filter name=$service_name)
+    IFS='/';
+    for num in $service_replicas; do
+        echo $num;
+        exit;
+    done
+    # service_replicas=$(docker service inspect --format='{{.Spec.Mode.Replicated.Replicas}}' $service_name)
     echo $service_replicas
 else
     echo 0
