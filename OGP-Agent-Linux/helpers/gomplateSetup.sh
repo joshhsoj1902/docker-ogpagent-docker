ce4548fff80cdc3fc55d4ac083a4f6186baf10f4
josh <joshhsoj1902@gmail.com>
2018-03-13 21:47:40 -0400

    more, better updates

--- OGP-Agent-Linux/helpers/gomplateSetup.sh
+++ OGP-Agent-Linux/helpers/gomplateSetup.sh
@@ -10,13 +10,14 @@ echo 'game dir: '$game_instance_dir
 echo 'homeId: '$home_id >> $game_instance_dir/home.yml
 
 configdatasource='config=file://'$game_instance_dir'/docker-config.yml';
+globalconfigdatasource='globalConfig=file:///opt/OGP/Cfg/Config.yml';
 homedatasource='home=file://'$game_instance_dir'/home.yml';
 binddatasource='bind=file:///opt/OGP/Cfg/bind.yml';
 template='/opt/OGP/templates/docker-compose.tmpl.yml';
 output=$game_instance_dir'/docker-compose.yml';
 
-echo 'GOMPLATE: ' -d $configdatasource -d $homedatasource -d $binddatasource -f $template -o $output
+echo 'GOMPLATE: ' -d $configdatasource -d $globalconfigdatasource -d $homedatasource -d $binddatasource -f $template -o $output
 
-/usr/local/bin/gomplate -d $configdatasource -d $homedatasource -d $binddatasource -f $template -o $output
+/usr/local/bin/gomplate -d $configdatasource -d $globalconfigdatasource -d $homedatasource -d $binddatasource -f $template -o $output
 
 cat $game_instance_dir'/docker-environment.yml' >> $output
