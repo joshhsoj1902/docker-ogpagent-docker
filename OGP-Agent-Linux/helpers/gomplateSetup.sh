1a7712c576f2f6e29bc8b0aafd4e08256bdab648
Josh Bryans <joshhsoj1902@gmail.com>
2018-03-24 20:36:45 -0400

    Volume support (#27)
    * test somethings
    
    * write env vars to file
    
    * use global config
    
    * more, better updates
    
    * try adding home id to mount
    
    * sub folder
    
    * missed }
    
    * cleanup
    
    * cleanup template file

--- OGP-Agent-Linux/helpers/gomplateSetup.sh
+++ OGP-Agent-Linux/helpers/gomplateSetup.sh
@@ -7,13 +7,17 @@ game_instance_dir=$game_dir/$home_id
 echo 'homeid: '$home_id
 echo 'game dir: '$game_instance_dir
 
+echo 'homeId: '$home_id >> $game_instance_dir/home.yml
+
 configdatasource='config=file://'$game_instance_dir'/docker-config.yml';
+globalconfigdatasource='globalConfig=file:///opt/OGP/Cfg/Config.yml';
+homedatasource='home=file://'$game_instance_dir'/home.yml';
 binddatasource='bind=file:///opt/OGP/Cfg/bind.yml';
 template='/opt/OGP/templates/docker-compose.tmpl.yml';
 output=$game_instance_dir'/docker-compose.yml';
 
-echo 'GOMPLATE: ' -d $configdatasource -d $binddatasource -f $template -o $output
+echo 'GOMPLATE: ' -d $configdatasource -d $globalconfigdatasource -d $homedatasource -d $binddatasource -f $template -o $output
 
-/usr/local/bin/gomplate -d $configdatasource -d $binddatasource -f $template -o $output
+/usr/local/bin/gomplate -d $configdatasource -d $globalconfigdatasource -d $homedatasource -d $binddatasource -f $template -o $output
 
 cat $game_instance_dir'/docker-environment.yml' >> $output
